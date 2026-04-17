1c9af1722f186c8fad550de62c137c7f386f1941
crmoore00 <51109069+crmoore00@users.noreply.github.com>
2026-04-17 11:16:46 -0500

    fixing deployment error

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import streamlit as st
 st.set_page_config(page_title='Economy Diconomy')
 
 pg = st.navigation([
-    st.Page("pages/Home.py", title="Home"),
+    st.Page("pages/home.py", title="Home"),
     st.Page("pages/cpi.py", title="CPI"),
     st.Page("pages/inflation.py", title="Inflation"),
     st.Page('pages/settings.py', title='Settings')
